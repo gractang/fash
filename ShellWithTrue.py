127f293dc2dedef4298252095eb6ac77c25b3800
jlutch <jonathan.lutch@menloschool.org>
2021-03-15 00:42:44 -0700

    Added Jobs command to ShellWithTrue file. A few things to note. 1. I changed the key word (temporarily) to jobs! so it would not mix with the unix one. 2. There is a random +1 added to a pid value - this is intentional. If using shell = true, the pid command gives you the pid of the.. yknow ill explain in person.. er.. discord

--- ShellWithTrue.py
+++ ShellWithTrue.py
@@ -2,12 +2,13 @@ import sys
 import os
 import subprocess
 import shlex
+import time
 
 PROMPT = "$ "
 EXIT = "exit"
 CD = "cd"
 PWD = "pwd"
-JOBS = "jobs"
+JOBS = "jobs!"
 BG = "bg"
 FG = "fg"
 HISTORY = "prev"
@@ -15,7 +16,7 @@ HIST_FILENAME = "history.txt"
 PIPE = " | "
 GOODBYE = "My battery is low and it's getting dark..."
 BUILTINS = [CD, PWD, JOBS, BG, FG, HISTORY, EXIT]
-
+processes = []
 
 def tash_cd(file_path):
 	try:
@@ -37,9 +38,15 @@ def builtins(uinput):
 		print(os.getcwd())
 		return os.getcwd()
 
-	if uinput == 'jobs':
+	if uinput == 'jobs!':
+		running_processes = [("pid", "cmd")]
+		for entry in processes:
+			if entry[0].poll() == None:
+				#ok so this plus one looks super arbitrary, but it is here for a reason
+				running_processes.append((entry[0].pid + 1, entry[1]))
+		for i in running_processes:
+			print(i)
 		return
-
 	if uinput == 'bg':
 		return
 
@@ -53,7 +60,7 @@ def exec():
 		uinput = input(PROMPT)
 		if len(uinput) != 0:
 			good_uin = True
-	print(uinput)
+	#print(uinput)
 	#MAKES THE LARGE ASSUMPTION THAT ANY BUILTINS ARE PASSED IN IN ISOLATION
 	if uinput.split()[0] in BUILTINS:
 				#run builtin function	
@@ -61,8 +68,11 @@ def exec():
 	else:
 		#commented out bc it kills subprocess commands
 		#shlex.quote()
-		p2 = subprocess.Popen(uinput, shell = True)
-		print(p2.pid)
+		p = subprocess.Popen(uinput, shell = True)
+		
+		global processes
+		processes.append((p, uinput))
+		#print(p.pid)
 				
 def main():
 	while(True):
