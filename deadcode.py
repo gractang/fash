00f22ac5e05fe81ccf3e5b260f648f3739fe7603
gractang <purple.grace.t@gmail.com>
2021-03-16 23:39:10 -0700

    deleted some files

--- deadcode.py
+++
@@ -1,9 +0,0 @@
-# if uin == EXIT:
-# 	return 0
-# # cd command (builtin)
-# if uin[:len(CD)] == CD:
-# 	# filepath
-# 	fp = uin[len(CD):]
-# 	tash_cd(fp)
-# else:
-# 	exc(uin)
--- shellForkTest.py
+++
@@ -1,7 +0,0 @@
-#this is a test progam
-
-import time
-
-print("Starting test!")
-time.sleep(2)
-print("Test ended!")
